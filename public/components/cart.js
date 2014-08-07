8490c43130ab3d405f167bf94b97f9588ed37083
wxx <wxx@xiaocong.tv>
2014-08-07 20:46:45 +0800

    fix 'car' -> cart

--- public/components/cart.js
+++ public/components/cart.js
@@ -1,17 +1,17 @@
-angular.module("car",[])
-.factory("car",function(){
-    var carData =[];
+angular.module("cart",[])
+.factory("cart",function(){
+    var cartData =[];
     return{
         addProduct: function(id,name,price){
             var addedToExistingItem = false;
-            for(var i = 0; i<carData.length;i++){
-                if(carDate[i].id == id){
+            for(var i = 0; i<cartData.length;i++){
+                if(cartDate[i].id == id){
                     carDateS[i].count++;
                     addedToExistingItem=true;
                 }
             }
             if(!addedToExistingItem){
-                carData.push({
+                cartData.push({
                     id:id,
                     count:1,
                     price:price,
@@ -20,15 +20,38 @@ angular.module("car",[])
             }
         },
         removeProduct:function(id){
-            for(var i=0 ;i<carData.length;i++){
-                if(carData[i].id ==id){
+            for(var i=0 ;i<cartData.length;i++){
+                if(cartData[i].id ==id){
                     cartData.splice(i,1);
                     break;
                 }
             }
         },
-        getProduct:function(){
+        getProducts:function(){
             return cartData;
         }
+    }
+})
+.directive("cartSummary",function(cart){
+    return{
+        restrict:"E",
+        templateUrl:"components/cart/cartSummary.html",
+        conroller:function($scope){
+            var cartData = cat.getProducts();
+            $scope.total= function(){
+                var total =0;
+                for(var i=0;i<cartData.length;i++){
+                    total+=(cartData[i].price * cartData[i].count);
+                }
+                return total;
+            };
+            $scope.itemCount=function(){
+                var total =0;
+                for(var i=0;i<cartData.length;i++){
+                    total+= cartData[i].count;
+                }
+                return total;
+            }
+        },
     };
 });
